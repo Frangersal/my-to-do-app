8e2a0da32bb55ee909a04effc3e8edba937b5bef
frangersal <frangersal@outlook.com>
2025-08-01 01:03:14 -0600

    feat: add Bootstrap for styling and enhance TodoApp layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './todoApp.tsx'
+import { TodoApp } from "./todoApp.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TodoApp />
   </StrictMode>,
 )
