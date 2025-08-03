4a3d469535d5dbd96b52424fad0496327868f27e
frangersal <frangersal@outlook.com>
2025-08-02 23:25:36 -0600

    feat: update language to Spanish and improve component structure in TodoApp by adding AddTask, ButtonSave, Task, ButtonCheck and ButtonDelete

--- src/todoApp.tsx
+++ src/todoApp.tsx
@@ -4,29 +4,97 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
-export const TodoApp = () => {
-  const [count, setCount] = useState(0)
+/// ===>>> Componente de AddTask / Añadir Tarea <<<===///
+const AddTask = ({ }) => {
+
 
   return (
-    //
     <>
-      {/* <div className="container-fluid bg-dark" style={{ backgroundColor: '#eee' }}>
-        <div className="card bg-primary-subtle">
-          <div className="card-header">
-            Featured
-          </div>
-          <div className="card-body">
-            <h5 className="card-title">Special title treatment</h5>
-            <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
+      <div className="col-12">
+        <div data-mdb-input-init className="form-outline">
+
+          <input type="text" id="form1" className="form-control" placeholder="Agregar tarea aqui..." />
         </div>
-      </div> */}
+      </div>
+
+      <div className="col-12">
+
+        <ButtonSave />
+
+      </div>
+    </>
+  )
+}
+
+/// ===>>> Componente de Save / Guardar Tarea <<<===///
+const ButtonSave = ({ }) => {
 
 
+  return (
+    <>
+      <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary">Guardar</button>
+    </>
+  )
+}
+
+/// ===>>> Componente de Task / Tarea <<<===///
+const Task = ({ }) => {
+
+
+  return (
+    <>
+      <tr>
+        <th scope="row">1</th>
+        <td>Buy groceries for next week</td>
+        <td>In progress</td>
+        <td>
+          <div className="btn-group" role="group" aria-label="Basic mixed styles example">
+
+            <ButtonCheck />
+
+            <ButtonDelete />
+
+          </div>
+        </td>
+      </tr>
+    </>
+  );
+}
+
+/// ===>>> Componente de Delete / Eliminar Tarea <<<===///
+const ButtonCheck = ({ }) => {
+
+
+  return (
+    <>
+      <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-success">Tachar</button>
+    </>
+  )
+}
+
+/// ===>>> Componente de Delete / Eliminar Tarea <<<===///
+const ButtonDelete = ({ }) => {
+
+
+  return (
+    <>
+      <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-danger">Eliminar</button>
+    </>
+  )
+}
+
+/// ===>>> Componente Padre <<<=== ///
+export const TodoApp = () => {
+
+  /// ===>>> States <<<=== ///
+  const [task, setTask] = useState([])
+
+  /// ===>>> Renderizado <<<=== ///
+  return (
+    <>
       <section className="vh-100" style={{
-    background: '#eee',
-  }}>
+        background: '#eee',
+      }}>
         <div className="container py-5 h-100">
           <div className="row d-flex justify-content-center align-items-center h-100">
             <div className="col col-lg-9 col-xl-7">
@@ -35,64 +103,24 @@ export const TodoApp = () => {
                   <h4 className="text-center my-3 pb-3">To Do App</h4>
 
                   <form className="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
-                    <div className="col-12">
-                      <div data-mdb-input-init className="form-outline">
-                        <input type="text" id="form1" className="form-control" placeholder="Enter a task here" />
-                      </div>
-                    </div>
 
-                    <div className="col-12">
-                      <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-primary">Save</button>
-                    </div>
+                    <AddTask />
 
-                    <div className="col-12">
-                      <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-warning">Get tasks</button>
-                    </div>
                   </form>
 
                   <table className="table mb-4  table-dark ">
                     <thead>
                       <tr>
                         <th scope="col">No.</th>
-                        <th scope="col">Todo item</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Actions</th>
+                        <th scope="col">Tareas</th>
+                        <th scope="col">Estatus</th>
+                        <th scope="col">Acciones</th>
                       </tr>
                     </thead>
                     <tbody>
-                      <tr>
-                        <th scope="row">1</th>
-                        <td>Buy groceries for next week</td>
-                        <td>In progress</td>
-                        <td>
-                          <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-                            <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-danger">Delete</button>
-                            <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-success">Finished</button>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th scope="row">2</th>
-                        <td>Renew car insurance</td>
-                        <td>In progress</td>
-                        <td>
-                          <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-                            <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-danger">Delete</button>
-                            <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-success">Finished</button>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th scope="row">3</th>
-                        <td>Sign up for online course</td>
-                        <td>In progress</td>
-                        <td>
-                          <div className="btn-group" role="group" aria-label="Basic mixed styles example">
-                            <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-danger">Delete</button>
-                            <button type="submit" data-mdb-button-init data-mdb-ripple-init className="btn btn-success">Finished</button>
-                          </div>
-                        </td>
-                      </tr>
+
+                      <Task />
+
                     </tbody>
                   </table>
                 </div>
